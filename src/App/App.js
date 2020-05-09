837277d4bbc9b647bfba2d258a9f96899bc8fef3
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-05-09 14:24:01 -0500

    setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>React Hoarder</h2>
+        <button className="btn btn-info">Too Much</button>
+      </div>
+    );
+  }
+}
+
+export default App;
