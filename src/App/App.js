6d58050383f3535285732a85f5383a2058d92719
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-06-09 21:20:37 -0500

    got auth to work

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Auth from '../components/pages/Auth/Auth';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+import fbConnection from '../helpers/data/connection';
+
+
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
+        <MyNavbar />
+        <Auth />
         <h2>React Hoarder</h2>
         <button className="btn btn-info">Too Much</button>
       </div>
     );
   }
 }
+
 
 export default App;
