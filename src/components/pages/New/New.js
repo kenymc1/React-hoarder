68229eec3fc8d10e85d6b320e75b8819515a4729
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-06-19 17:59:25 -0500

    added view and edit buttons

--- src/components/pages/New/New.js
+++ src/components/pages/New/New.js
@@ -2,9 +2,40 @@ import React from 'react';
 import './New.scss';
 
 class New extends React.Component {
+  state = {
+    itemName: '',
+  }
+
+  itemChange = (e) => {
+    e.preventDefault();
+    this.setState({ itemName: e.target.vale });
+  }
+
   render() {
+    const {
+      itemName,
+    } = this.state;
     return (
-      <h1 className="New">New</h1>
+      <div className="new col-12">
+
+      <h1>New</h1>
+      <form className=" col-6 offset-3 text-left">
+
+        <div class="form-group">
+          <label for="exampleInputEmail1">Item Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="item-itemName"
+            value={itemName}
+            onChange={this.itemChange}
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+      </div>
     );
   }
 }
--- src/components/shared/ItemCard/ItemCard.js
+++ src/components/shared/ItemCard/ItemCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import itemShape from '../../../helpers/propz/itemShape';
 import './ItemCard.scss';
@@ -11,12 +12,16 @@ class ItemCard extends React.Component {
 
   render() {
     const { item } = this.props;
+    const singleLink = `/stuff/${item.id}`;
+    const editLink = `/edit/${item.id}`;
     return (
       <div className="ItemCard col-3">
         <div className="card">
         <div className="card-body">
             <img className="img" width="100%" src={item.itemImage} alt="Logo" />
             <h5 className="card-title">{item.itemName}</h5>
+            <Link className="btn btn-info" to={singleLink}><i className="fas fa-binoculars"></i></Link>
+            <Link className="btn btn-warning" to={editLink}><i className="fas fa-edit"></i></Link>
             <p className="card-text">{item.itemDescription}</p>
           </div>
         </div>
