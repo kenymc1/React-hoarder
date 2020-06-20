5cfbac13005d882f93be1b46e522a12337944626
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-06-20 08:24:48 -0500

    added second delete to single view

--- src/components/pages/SingleItem/SingleItem.js
+++ src/components/pages/SingleItem/SingleItem.js
@@ -16,10 +16,19 @@ class SingleItem extends React.Component {
       .catch((err) => console.error('unable to get single items:', err));
   }
 
-  render() {
-    const { item } = this.state;
-    return (
+
+removeItem = () => {
+  const { itemId } = this.props.match.params;
+  itemData.deleteItem(itemId)
+    .then(() => this.props.history.push('/home'))
+    .catch((err) => console.error('unable to delete scat: ', err));
+}
+
+render() {
+  const { item } = this.state;
+  return (
       <div className='SingleStuff'>
+        <button className="btn btn-danger" onClick={this.removeItem}><i className="fas fa-trash-alt"></i></button>
         <h1 className='mt-3 text-center'>Single Stuff</h1>
         <div className='row justify-content-center'>
           <div className='card col-6 mb-5'>
@@ -33,7 +42,7 @@ class SingleItem extends React.Component {
           </div>
         </div>
       </div>
-    );
-  }
+  );
+}
 }
 export default SingleItem;
